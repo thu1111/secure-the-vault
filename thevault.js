129257de528984efa366b4afce4728034469d183
Thu Yein Tun <yon.fu111@gmail.com>
2024-11-04 12:42:08 -0500

    initial commit

---
+++ thevault.js
@@ -0,0 +1,25 @@
+/* Pseudocodes
+1. create a variable userString to store a message to user.
+2. create three number variables and assign each with the result of calculation using a unique arithmetic operator.
+3. (optional)create a variable outputString that stores the desired message with all the variables properly arranged
+4. show user the outputSting with alrt()
+*/
+
+
+//a variable assigned with message string for user
+const userString = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+//assign num1 with calculation that results the number 10
+const num1 = 40/4;
+
+//assign num1 with calculation that results the number 40
+const num2 = 25+15;
+
+//assign num1 with calculation that results the number 39
+const num3 = 45-6;
+
+//combine all the message and number combination
+let outputString = `${userString} ${num1} - ${num2} - ${num3}`;
+
+//print message and number combination to the user 
+alert(outputString);
